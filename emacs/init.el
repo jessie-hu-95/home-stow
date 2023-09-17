ae6f3b29fa525f2f5a5d33359e210867813d2837
Jessie Hu <jessie.hu.95@icloud.com>
2023-09-17 14:08:08 +0800

    emacs: more detailed configuration on modus theme

--- emacs/init.el
+++ emacs/init.el
@@ -102,27 +102,51 @@
   (display-graphic-p)
   :custom
   (menu-bar-mode t)
-  (default-frame-alist '((fullscreen . maximized)))
-  :config
-  (set-face-attribute 'default nil :height 130))
+  (default-frame-alist '((fullscreen . maximized))))
 
 (use-package emacs
   :when
   (and (display-graphic-p)
-       (find-font (font-spec :name "Lucida Grande Mono DK")))
-  :config
-  (set-face-attribute 'default nil :font "Lucida Grande Mono DK"))
+       (find-font (font-spec :name "Lucida Grande Mono DK"))
+       (find-font (font-spec :name "Lucida Sans OT")))
+  :custom-face
+  (default        ((t (:family "Lucida Grande Mono DK" :height 130))))
+  (fixed-pitch    ((t (:family "Lucida Grande Mono DK"))))
+  (variable-pitch ((t (:family "Lucida Sans OT" :height ,(/ 1.0 0.92))))))
 
 (use-package emacs
   :when
   (display-graphic-p)
+  :init
+  (defconst jess/one-golden-ratio (+ (/ (- (sqrt 5) 1) 2) 1))
+  ;; Set the height of level-0 headings to `one-golden-ratio' times to
+  ;; the default height.  Heading of each level decreases
+  ;; expotentially until the 5th.
+  (defconst jess/1gr-6th-rt (expt jess/one-golden-ratio (/ 1.0 6.0)))
   :custom
   (modus-themes-mode-line '(accented))
   (modus-themes-hl-line '(accented intense))
+  (modus-themes-fringes nil)
+  (modus-themes-region '(no-extend bg-only accented))
   (modus-themes-bold-constructs t)
   (modus-themes-italic-constructs t)
+  (modus-themes-mixed-fonts t)
+  (modus-themes-deuteranopia t)
+  ;; The `faint' property fades color saturation saturations.  An MIT
+  ;; course suggests to avoid saturated colors in UI color design
+  ;; (https://web.mit.edu/6.813/www/sp16/classes/16-color/#design-guidelines).
+  (modus-themes-syntax '(faint alt-syntax green-strings yellow-comments))
+  (modus-themes-diffs 'desaturated)
   (modus-themes-completions '((selection . (intense accented))
 			      (popup . (intense accented))))
+  (modus-themes-headings
+   `((5 . (variable-pitch ,(expt jess/1gr-6th-rt 1) semibold))
+     (4 . (variable-pitch ,(expt jess/1gr-6th-rt 2) semibold))
+     (3 . (variable-pitch ,(expt jess/1gr-6th-rt 3) bold))
+     (2 . (variable-pitch ,(expt jess/1gr-6th-rt 4) bold))
+     (1 . (variable-pitch ,(expt jess/1gr-6th-rt 5) heavy))
+     (0 . (variable-pitch ,(expt jess/1gr-6th-rt 6) heavy))
+     (t . (variable-pitch 1.0))))
   :config
   (load-theme 'modus-operandi))
 
