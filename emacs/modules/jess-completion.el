92c774dd81bd1419c109339b4bd1063c64f8e6a4
Jessie Hu <jessie.hu.95@icloud.com>
2023-09-22 15:03:11 +0800

    emacs: fix missing `cape-elisp-symbol'

--- emacs/modules/jess-completion.el
+++ emacs/modules/jess-completion.el
@@ -97,7 +97,7 @@
   (add-to-list 'completion-at-point-functions #'cape-history)
   (add-to-list 'completion-at-point-functions #'cape-keyword)
   (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+  (add-to-list 'completion-at-point-functions #'cape-symbol)
   (add-to-list 'completion-at-point-functions #'cape-elisp-block)
   (add-to-list 'completion-at-point-functions #'cape-tex))
 
