a4d1987df1bd0820e9cfd8aa8ff8d7eca993e792
Jessie Hu <jessie.hu.95@icloud.com>
2023-09-25 15:49:53 +0800

    emacs: disable column and line number in shells

--- emacs/modules/jess-shell.el
+++ emacs/modules/jess-shell.el
@@ -21,14 +21,22 @@
 
   :hook
   ;; Disable `corfu-auto' in (e)shell-mode, since `RET' is both used
-  ;; by `corfu-insert' and `comint-send-input'
-  (shell-mode  . (lambda () (setq-local corfu-auto nil)))
-  (eshell-mode . (lambda () (setq-local corfu-auto nil)))
+  ;; by `corfu-insert' and `comint-send-input'.  Disable
+  ;; `global-hl-line-mode', `column-number-mode' and
+  ;; `line-number-mode' in `eat-', `shell-' and `eshell-' modes.
+  (shell-mode  . (lambda () (setq-local corfu-auto          nil
+                                        global-hl-line-mode nil
+                                        column-number-mode  nil
+                                        line-number-mode    nil)))
+  (eshell-mode . (lambda () (setq-local corfu-auto          nil
+                                        global-hl-line-mode nil
+                                        column-number-mode  nil
+                                        line-number-mode    nil)))
 
   ;; Disable `global-hl-line-mode' in each shell/terminal
-  (shell-mode  . (lambda () (setq-local global-hl-line-mode nil)))
-  (eshell-mode . (lambda () (setq-local global-hl-line-mode nil)))
-  (eat-mode    . (lambda () (setq-local global-hl-line-mode nil))))
+  (eat-mode    . (lambda () (setq-local global-hl-line-mode nil
+                                        column-number-mode  nil
+                                        line-number-mode    nil))))
 
 
 (provide 'jess-shell)
